4db308f653754e94e28691a95becc3c30f3285a1
Joe Stead <joestead1@googlemail.com>
2016-06-27 20:47:30 +0100

    Ignore, attributes and stub projects

---
+++ TodoApi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy.Hosting.Self;
+
+namespace TodoApi
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      using (var host = new NancyHost(new Uri("http://localhost:4204")))
+      {
+        host.Start();
+        Console.WriteLine("Started!");
+        Console.ReadLine();
+      }
+    }
+  }
+}
+
+
